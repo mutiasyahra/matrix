e9b61f8a9fbb7b08a3b70eef14bffa449ac578ab
mutiasyahra <133612425+mutiasyahra@users.noreply.github.com>
2024-03-20 14:54:57 +0700

    Add files via upload

---
+++ Matrix Algebra/2_matrix_subtraction.py
@@ -0,0 +1,24 @@
+x = [
+    [1,2,3],
+    [9,5,2],
+    [8,4,7]
+]
+
+y = [
+    [1,5,2],
+    [3,4,1],
+    [6,2,3]
+]
+
+result = [
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+]
+
+for i in range(len(x)):
+    for j in range(len(x[0])):
+        result[i][j] = x[i][j] - y[i][j]
+
+for r in result:
+    print(r)
